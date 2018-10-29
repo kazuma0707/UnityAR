55f5aeddad833c0605e0c07239e3af87a6ab4700
yuya <yuya.okada.program@gmail.com>
2018-10-29 16:28:16 +0900

    鑑賞シーンのボタン追加

---
+++ UnityAR/Assets/Yuya/#Script/PoseButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseButtonController : MonoBehaviour
+{
+    private bool isDisplay = false;
+    [SerializeField]
+    private GameObject PoseScrollView;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+
+    public void Onclick()
+    {
+        if(!isDisplay)
+        {
+            PoseScrollView.SetActive(true);
+            isDisplay = true;
+        }
+        else
+        {
+            PoseScrollView.SetActive(false);
+            isDisplay = false;
+        }
+    }
+}
