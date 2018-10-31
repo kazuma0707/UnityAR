22a1745ce867c0469bc16cff4791b63ec0a93b15
yuya <yuya.okada.program@gmail.com>
2018-10-31 09:20:06 +0900

    鑑賞シーンのポーズ追加

---
+++ UnityAR/Assets/Yuya/#Script/PoseController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseController : MonoBehaviour
+{
+
+    public Animator anim; // 対象のAnimatorコンポーネント
+
+    public void ChangePose(string pose)
+    {
+        int hash = Animator.StringToHash(pose);//ポーズ名
+        anim.Play(hash, -1, 0);//ハッシュ、レイヤー、正規化された時間(0-1)
+    }
+}
