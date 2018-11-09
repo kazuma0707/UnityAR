f326e8cc7b661f2fb890c9eaf034d89b018260fd
Ka2019 <kkam0707@gmail.com>
2018-11-09 09:33:54 +0900

    ランタイムパーミッションの実装

---
+++ UnityAR/Assets/#Script/Permission/RuntimePermission.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RuntimePermission : MonoBehaviour {
+    private string permission = "android.permission.WRITE_EXTERNAL_STORAGE";
+    private bool permissionRequested = false;
+
+    void Start()
+    {
+        if (!RuntimePermissionHelper.HasPermission(permission))
+        {
+            if (RuntimePermissionHelper.ShouldShowRequestPermissionRationale(permission))
+            {
+                // パーミッションを要求する意味を説明するUIを表示
+            }
+            else
+            {
+                // パーミッションをリクエスト
+                RuntimePermissionHelper.RequestPermission(new string[] { permission });
+                permissionRequested = true;
+            }
+        }
+    }
+
+    // パーミッションダイアログから戻ってきたときなどに呼ばれる
+    void OnApplicationPause(bool pauseStatus)
+    {
+        // ポーズからの復帰時かつパーミッションリクエストの直後の場合
+        if (!pauseStatus && permissionRequested)
+        {
+            // パーミッションを持っているかどうか
+            if (RuntimePermissionHelper.HasPermission(permission))
+            {
+                Debug.Log("パーミッションリクエスト成功!");
+                permissionRequested = false;
+            }
+        }
+    }
+}
