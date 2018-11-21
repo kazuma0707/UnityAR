4eb10eb127ca91b29028b978a0a3292872a33c5e
Ka2019 <kkam0707@gmail.com>
2018-11-21 12:06:01 +0900

    アスペクト比の検証

--- UnityAR/Assets/GoogleARCore/Examples/HelloAR/Scripts/HelloARController.cs
+++ UnityAR/Assets/GoogleARCore/Examples/HelloAR/Scripts/HelloARController.cs
@@ -83,7 +83,7 @@ namespace GoogleARCore.Examples.HelloAR
 
 
             _UpdateApplicationLifecycle();
-            //生成オブジェクトの回転
+            //生成オブジェクトの回転（生成時に常に自分を向くようにする）
             if (unityChanObject != null)
             {
                 var aim = Camera.main.transform.position - unityChanObject.transform.position;
@@ -126,13 +126,14 @@ namespace GoogleARCore.Examples.HelloAR
             bool showSearchingUI = true;
             for (int i = 0; i < m_AllPlanes.Count; i++)
             {
+                //水平面の検知ができたら
                 if (m_AllPlanes[i].TrackingState == TrackingState.Tracking)
                 {
                     showSearchingUI = false;
                     break;
                 }
             }
-
+            //水平面検知中のUIを非表示
             SearchingForPlaneUI.SetActive(showSearchingUI);
 
             // If the player has not touched the screen, we are done with this update.
