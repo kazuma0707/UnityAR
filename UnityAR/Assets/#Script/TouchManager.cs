9af88d7a9a6af43e8f70919048fc44e40045fd98
Ka2019 <kkam0707@gmail.com>
2018-11-30 14:05:21 +0900

    ビューフォリアの追加

---
+++ UnityAR/Assets/#Script/TouchManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+
+    public GameObject m_tapEffect;
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TouchInfo info=AppUtil.GetTouch();
+        if(info==TouchInfo.Began)
+        {
+            Debug.Log(AppUtil.GetTouchWorldPosition(Camera.main));
+            NewTapEffect(AppUtil.GetTouchPosition());
+        }
+        
+		
+	}
+    void NewTapEffect(Vector3 pos)
+    {
+        
+        Instantiate(m_tapEffect, pos, Quaternion.identity, transform);
+    }
+}
